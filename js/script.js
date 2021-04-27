82274afc622eb64209a75f18e58a8f7a3caa56b6
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-27 13:53:50 +0200

    inizio con commenti

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// chiedo all’utente il numero di chilometri che vuole percorrere
+
+
+// chiedo la sua età
+
+
+// stabilisco il costo del biglietto in base ai km inseriti dall'utente (0.21 euro al km)
+
+
+// stabilisco gli sconti
+
+
+// stabilisco un vincolo
+
+
+// creo vincolo sconto da vedere nel riepilogo dati
+
+
+// stampo il costo del biglietto
+
+
+// stampo il riepilogo Dati
