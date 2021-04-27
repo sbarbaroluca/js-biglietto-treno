5edb1526c99a8e7417dcca4e6470c557d824509e
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-27 14:43:55 +0200

    html + km e età

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // chiedo all’utente il numero di chilometri che vuole percorrere
-
+var chilometriTragitto = parseInt(prompt("Quanti chilometri devi percorrere?"));
+if (chilometriTragitto) {
+  alert("Puoi inserire solo numeri");
+} else {
 
 // chiedo la sua età
-
+var etaPasseggero = parseInt(prompt("Quanti anni hai?"));
+  if (etaPasseggero) {
+    alert("Puoi inserire solo numeri");
+  } else {
 
 // stabilisco il costo del biglietto in base ai km inseriti dall'utente (0.21 euro al km)
 
@@ -19,4 +25,3 @@
 // stampo il costo del biglietto
 
 
-// stampo il riepilogo Dati
