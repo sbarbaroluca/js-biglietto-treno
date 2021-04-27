2a832474c3d6cd08baa80944c43a8946d5bdfaae
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-27 16:46:57 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // chiedo all’utente il numero di chilometri che vuole percorrere
 var chilometriTragitto = parseInt(prompt("Quanti chilometri devi percorrere?"));
-if (chilometriTragitto) {
+if (isNaN(chilometriTragitto)) {
   alert("Puoi inserire solo numeri");
 } else {
 }
 
 // chiedo la sua età
 var etaPasseggero = parseInt(prompt("Quanti anni hai?"));
-if (etaPasseggero) {
+if (isNaN(etaPasseggero)) {
   alert("Puoi inserire solo i numeri");
 } else {
 }
@@ -21,21 +21,28 @@ var scontoVenti = (costoBiglietto * 20) / 100;
 var scontoQuaranta = (costoBiglietto * 40) / 100;
 
 // stabilisco un vincolo
-if (chilometriTragitto) || isNaN(etaPasseggero) 
+if (isNaN(chilometriTragitto) || isNaN(etaPasseggero)) {
   alert("Puoi inserire solo i numeri");
 } else {
-
-}
   if (etaPasseggero < 18) {
     costoBiglietto = costoBiglietto - scontoVenti;
     var scontoApplicato = scontoVenti;
-  } 
-  } else 
-  
+  } else if (etaPasseggero > 65) {
+    costoBiglietto = costoBiglietto - scontoQuaranta;
+    scontoApplicato = scontoQuaranta;
 
+  } else {
+    costoBiglietto;
+    scontoApplicato = 0;
+  }
+
+}
 
 // stampo il costo del biglietto
+document.getElementById("costo-biglietto").innerHTML = costoBiglietto.toFixed(2);
 
 // stampo il riepilogo Dati
-d
+document.getElementById("chilometri-tragitto").innerHTML = chilometriTragitto;
+document.getElementById("eta-passeggero").innerHTML = etaPasseggero;
+document.getElementById("sconto-applicato").innerHTML = scontoApplicato.toFixed(2);
   
