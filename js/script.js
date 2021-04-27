66e00612f9ceb27fb5a70b03dabc8c02a75bbf0c
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-04-27 16:29:10 +0200

    fino a sconti

--- js/script.js
+++ js/script.js
@@ -3,25 +3,39 @@ var chilometriTragitto = parseInt(prompt("Quanti chilometri devi percorrere?"));
 if (chilometriTragitto) {
   alert("Puoi inserire solo numeri");
 } else {
+}
 
 // chiedo la sua età
 var etaPasseggero = parseInt(prompt("Quanti anni hai?"));
-  if (etaPasseggero) {
-    alert("Puoi inserire solo numeri");
-  } else {
+if (etaPasseggero) {
+  alert("Puoi inserire solo i numeri");
+} else {
+}
 
 // stabilisco il costo del biglietto in base ai km inseriti dall'utente (0.21 euro al km)
-
+var costoUnChilometro = 0.21;
+var costoBiglietto = chilometriTragitto * costoUnChilometro;
 
 // stabilisco gli sconti
-
+var scontoVenti = (costoBiglietto * 20) / 100;
+var scontoQuaranta = (costoBiglietto * 40) / 100;
 
 // stabilisco un vincolo
+if (chilometriTragitto) || isNaN(etaPasseggero) 
+  alert("Puoi inserire solo i numeri");
+} else {
 
-
-// creo vincolo sconto da vedere nel riepilogo dati
+}
+  if (etaPasseggero < 18) {
+    costoBiglietto = costoBiglietto - scontoVenti;
+    var scontoApplicato = scontoVenti;
+  } 
+  } else 
+  
 
 
 // stampo il costo del biglietto
 
-
+// stampo il riepilogo Dati
+d
+  
